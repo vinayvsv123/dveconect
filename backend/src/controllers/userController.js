641860d2ae9960e926579c4b1e019e2923b04ce2
vinay <vedulasaivinay@gmail.com>
2026-02-01 23:22:38 +0530

    created user controlller

---
+++ backend/src/controllers/userController.js
@@ -0,0 +1,66 @@
+import User from '../models/User.js';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+// Register a new user
+export const registerUser=async(req,res)=>{
+    try{
+            const{username,email,password}=req.body;
+            if(!username||!email||!password){
+                    return res.status(400).json({message:"Please provide all required fields"});
+            }
+            //if(password.)
+            const existingUser=await User.findOne({email});
+            if(existingUser)
+            {
+                return res.status(409).json({message:"user already exists"});
+            }
+            //const
+            const hashedPassword=await bcrypt.hash(password,10);
+            const newUser=new User({
+                username,
+                email,
+                password:hashedPassword,
+
+            });
+            await newUser.save();
+            res.status(201).json({message:"User registered successfully"});
+       }
+     catch(error)
+     {
+           console.error("Error registering user:",error);
+           res.status(500).json({message:"Server error"});
+     }
+
+    }
+
+    //login 
+    export const loginuser=async(req,res)=>{
+        try{
+            const{email,password}=req.body;
+            if(!email||!password)
+            {
+                return res.status(400).json({message:"Please provide all required fields"});
+            }
+
+            const user=await User.findOne({email});
+            //checking the user
+            if(!user)
+            {
+                return res.status(401).json({message:"Invalid credentials"});
+            }
+            const isPasswordValid=await bcrypt.compare(password,user.password);
+            //password matching
+            if(!isPasswordValid)
+            {
+                return res.status(401).json({message:"Invalid credentials"});
+            }
+
+
+        }
+        catch(error)
+        {
+            console.error("Error logging in user:",error);
+            res.status(500).json({message:"Server error"});
+        }
+    }
