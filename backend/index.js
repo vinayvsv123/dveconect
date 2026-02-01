2f961bd7d8a73ce5575d66c8a83933b28be966a7
vinay <vedulasaivinay@gmail.com>
2026-02-01 20:10:24 +0530

    setup backend structure, connect MongoDB"

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,9 @@
+import dotenv from "dotenv";
+import connectDB from "./src/config/db.js";
 import app from './app.js';
+
+dotenv.config();  // load .env
+connectDB();      // connect MongoDB
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
